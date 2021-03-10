9f8f463a3a166d80299f20580c143543b99eddf6
Jonathan Maron <jonathan.maron@theimagingsource.com>
2021-03-10 12:57:25 +0100

    Refactoring

--- src/HttpException/OutOfBoundsException.php
+++ src/HttpException/OutOfBoundsException.php
@@ -3,8 +3,6 @@ declare(strict_types=1);
 
 namespace Ctw\Http\HttpException;
 
-use OutOfBoundsException as ParentOutOfBoundsException;
-
-class OutOfBoundsException extends ParentOutOfBoundsException
+class OutOfBoundsException extends \OutOfBoundsException
 {
 }
